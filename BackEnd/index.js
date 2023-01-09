b01869c1e3a1198c6dff742a2d4e7aa798ec8b74
Rushikesh7997 <pardhikar26@gmail.com>
2023-01-09 23:37:18 +0530

    mongo connected

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,10 +6,15 @@ const app = express();
 mongoose.set("strictQuery", true);
 mongoose
   .connect(
-    "mongodb+srv://Rishi:RPKIvd6QltVc7pl4@cluster0.gj6qy9r.mongodb.net/bookStore?retryWrites=true&w=majority"
+    "mongodb+srv://admin:CeMvZP6DGipop03d@cluster0.vvqt6br.mongodb.net/book_store?retryWrites=true&w=majority"
   )
   .then(() => console.log("connected to Database"))
   .then(() => {
     app.listen(5000);
   })
   .catch((err) => console.log(err));
+
+
+
+
+  // CeMvZP6DGipop03d
